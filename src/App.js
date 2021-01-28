b47bb15c9452bd69abad91572d79c01cfb69ffe6
Kim Jae Hyeon <73218463+hanyugeon@users.noreply.github.com>
2021-01-28 19:49:02 +0900

    commit_06_component01

--- src/App.js
+++ src/App.js
@@ -1,12 +1,50 @@
 import React, { Component } from 'react';
-// import logo from './logo.svg';
 import './App.css';
 
-class App extends Component {
+// 컴포넌트(Component를 만드는 부분)
+class Subject extends Component { // Subject라는 Component를 만들겠다.
+  render() {
+    return (
+      <header>
+          <h1>WEB</h1>
+          world wide web!
+      </header>
+    );
+  }
+}
+
+class TOC extends Component {
+  render() {
+    return(
+      <nav>
+          <ul>
+            <li><a href="1.html">HTML</a></li>
+            <li><a href="1.html">CSS</a></li>
+            <li><a href="1.html">JavaScript</a></li>
+          </ul>
+      </nav>
+    );
+  }
+}
+
+class Content extends Component {
+  render() {
+    return (
+      <article>
+        <h2>HTML</h2>
+        HTML is HyperText Markup Language.
+      </article>
+    );
+  }
+}
+
+class App extends Component { // 유사 JS, JS가 아니다.
   render() {
     return (
       <div className="App">
-        Hello React!
+        <Subject></Subject>
+        <TOC></TOC>
+        <Content></Content>
       </div>
     );
   }
