9abc10c2cb9e97804bf385e0290bc59282cd80b6
hanyugeon <hanyugeon@naver.com>
2021-02-01 18:08:28 +0900

    commit_08_component02

--- src/App.js
+++ src/App.js
@@ -1,42 +1,8 @@
 import React, { Component } from 'react';
+import Content from "./components/Content";
+import TOC from "./components/TOC";
+import Subject from "./components/Subject";
 import './App.css';
-
-// 컴포넌트(Component를 만드는 부분)
-class Subject extends Component { // Subject라는 Component를 만들겠다.
-  render() {
-    return (
-      <header>
-          <h1>{this.props.title}</h1>
-          {this.props.sub}
-      </header>
-    );
-  }
-}
-
-class TOC extends Component {
-  render() {
-    return(
-      <nav>
-          <ul>
-            <li><a href="1.html">HTML</a></li>
-            <li><a href="1.html">CSS</a></li>
-            <li><a href="1.html">JavaScript</a></li>
-          </ul>
-      </nav>
-    );
-  }
-}
-
-class Content extends Component {
-  render() {
-    return (
-      <article>
-        <h2>{this.props.title}</h2>
-        {this.props.desc}
-      </article>
-    );
-  }
-}
 
 class App extends Component { // 유사 JS, JS가 아니다.
   render() {
